c3c3059ad83f92cc81221706103ff198b28ca96f
yusufbek-sheyh <yusufbek190991@gmail.com>
2022-03-11 17:36:45 +0500

    add

--- task2/index.js
+++ task2/index.js
@@ -1,62 +1,83 @@
-var users = {
-    // ozgaruvchilar
-    "Akmal": {
-        name: "Akmal",
-        surname: "O'ktamov",
-        age: 27,
-        salary: 800
+var Users = {
+    // 
+
+    yusuf :  {
+    name : "yusuf",
+    userName: "nuriddinov",
+    salary : 326 ,
+    age : 15,
     },
-
-    "Oybek": {
-         name: "Oybek",
-        surname: "sattorov",
-        age: 36,
-        salary: 1500
-    },
-
-    "Umid": {
-        name: "Umid",
-        surname: "Abdushukurov",
-        age: 21,
-        salary: 400
-   }
-//    ozgaruvchilar
-
-}
-
-function samSalary(users) {
-
-    var akmalSalary = users.Akmal.salary
-    var uybeksalary = users.Oybek.salary
-    var umidSalary = users.Umid.salary
-
-      var allUsersSalery  =  (akmalSalary + uybeksalary + umidSalary)
-
- //     console.log(akmalSalary, uybeksalary, umidSalaru, allUsersSelery);
-
-       return allUsersSalery //Bu yerga userlarni o'rtasha yoshini chiqarasiz
- }
-
-
-
-function sumage(users) {
     
-   var allAge =  ( users.Akmal.age  +  users.Oybek.age + users.Umid.age) / 3
+    // 
 
-    return allAge
+    Akmal: {
+    name: "Akmal",
+    userName: "O'ktamov",
+    salary: 800,
+    age: 27,
+    },
+
+    // 
+
+    Oybek: {
+    name: "Oybek",
+    userName: "Sattorov",
+    salary:1500,
+    age: 36,
+    },
+
+    // 
+    
+    Umid: {
+    name: "Umid",
+    userName: "Abdushukurov",
+    salary: 400,
+    age: 21,
+    },
+
+    // 
+
+};
+// var end
+
+
+// function start
+
+function sumSalary(Users) {
+    
+    var salary = 0;
+
+    for (var user in Users) {
+    salary += Users[user].salary;
+    }
+
+    return salary;
 }
+// fuctione nd
 
- console.log( sumage(users));
+// fuctione start
+
+    function averageAge(Users) {
+
+            var age = 0;
+            var length = 0;
+
+            for (var user in Users) {
+            length++;
+            age = age + Users[user].age;
+            }
+
+            return age / length;
+            
+    }
+
+    // fuctione nd
 
 
- 
-        console.log(samSalary(users));
 
+// concole 
 
-// console'dagi natija
-//  console.log(sumSalary(users));
-//--> 2700
+console.log(sumSalary(Users));
+console.log(averageAge(Users));
 
-//  console.log(avarageAge(users));
-//--> 28
-
+// concole end
